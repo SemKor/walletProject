624f56be64debff2a797d0772aed853e85a382a5
Cемён <semenkorolkov999@gmail.com>
2026-02-19 15:10:53 +0400

    added repository WalletRepository

---
+++ src/main/java/com/project/wallet/repository/WalletRepository.java
@@ -0,0 +1,11 @@
+package com.project.wallet.repository;
+
+import com.project.wallet.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+}
