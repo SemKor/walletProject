6b91ce1f7fca3bfc70e2cefb035fbd3ead6f624f
Cемён <semenkorolkov999@gmail.com>
2026-02-19 15:04:37 +0400

    added model Wallet

---
+++ src/main/java/com/project/wallet/model/Wallet.java
@@ -0,0 +1,44 @@
+package com.project.wallet.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "wallet")
+public class Wallet {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    public Wallet() {
+    }
+
+    public Wallet(UUID id, BigDecimal balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
